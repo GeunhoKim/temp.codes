5edd37c37cb86a5b75cc6c85b3a001900a4965ac
Geunho Khim <gkhim@ebay.com>
2015-12-12 18:26:20 +0900

    add annotation

--- js/removeUrlParams.js
+++ js/removeUrlParams.js
@@ -1,3 +1,12 @@
+/**
+ * url string의 query value만 제거한 url string을 반환
+ *
+ *   e.g. in: 'http://example.org/sample?member=admin&pageIdx=3'
+ *       out: 'http://example.org/sample?member&pageIdx'
+ *
+ *  요청 url에서 고유한 url 정보를 가져올 때 사용
+ *
+ */
 function removeUrlParams(url){
     var pattern = new RegExp(/(\?|\&)([^=]+)\=(.([^\&]+)|([0-9]))/g)
     var returnUrl = url;
